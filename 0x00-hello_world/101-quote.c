396b75efc0f1a710f572f31fdc7412d7fc741d53
Bode <akinbode.adeboyejo@gmail.com>
2023-06-18 06:38:37 -0700

    modified task 6 and extra task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
  * main - Entry point
@@ -5,11 +6,7 @@
  */
 int main(void)
 {
-	/**
-	 * prints exactly 'and that piece of art is useful
-	 * " - Dora Korpar, 2015-10-19'
-	 */
-	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar
-			, 2015-10-19\n", 59);
+	write(STDOUT_FILENO, "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -20,5 +20,6 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", b);
 	printf("Size of a long int: %d byte(s)\n", c);
 	printf("Size of a long long int: %d byte(s)\n", d);
+	printf("Size of a float: %d byte(s)\n", e);
 	return (0);
 }
