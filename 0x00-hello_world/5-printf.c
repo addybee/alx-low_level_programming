8de10769e3671ef904417cd6cc4db75ff51ae921
Bode <akinbode.adeboyejo@gmail.com>
2023-06-18 05:51:14 -0700

    modified task 5 and 6 and did extra task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0 (Success)
  */
-main(void)
+int main(void)
 {
 	/* print a string to the output */
 	printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,7 +9,6 @@ int main(void)
 {
 	/* declare  int variable to hold the byte each data type */
 	int a, b, c, d, e;
-	
 	/* assign values to the variables declared */
 	a = sizeof(char);
 	b = sizeof(int);
@@ -21,6 +20,5 @@ int main(void)
 	printf("Size of an int: %d byte(s)\n", b);
 	printf("Size of a long int: %d byte(s)\n", c);
 	printf("Size of a long long int: %d byte(s)\n", d);
-	
 	return (0);
 }
