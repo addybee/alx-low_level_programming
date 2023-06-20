9ec0ddc6d1324c40c1d31f64665880bec116ca69
Bode <akinbode.adeboyejo@gmail.com>
2023-06-19 22:53:35 -0700

    modify task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,10 +7,11 @@
 int main(void)
 {
 	int digit;
-
-	for (digit = 0; digit < '10'; digit++)
+        digit = '0';  
+	while (digit <= '9')
 	{
 		putchar(digit);
+		digit++;
 	}
 	putchar('\n');
 	return (0);
