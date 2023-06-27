d8ff410fc12da97efc816e5ac32e3f068bd86fba
Bode <akinbode.adeboyejo@gmail.com>
2023-06-27 07:43:39 -0700

    modified task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,26 +10,15 @@ void puts_half(char *str)
 
 	i = 0;
 	while (str[i] != '\0')
-	{
 		i++;
-	}
 	if ((i % 2 == 0))
-	{
 		j = i / 2;
-		while (j < i)
-		{
-			_putchar(str[j]);
-			j++;
-		}
-	}
 	else
+		j = (i + 1) / 2;
+	while (j < i)
 	{
-		j = (i - 1) / 2;
-		while (j < i)
-		{
-			_putchar(str[j]);
-			j++;
-		}
+		_putchar(str[j]);
+		j++;
 	}
 	_putchar('\n');
 }
