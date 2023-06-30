c4b61b6be1aef98895dc90d0c96e7077c8e864dc
Bode <akinbode.adeboyejo@gmail.com>
2023-06-30 15:49:06 -0700

    completed task 101 and 100

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n: the integer to be printed
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	int i, digit, div, t;
+
+	t = n;
+	digit = 1;
+	while (t /= 10)
+		digit++;
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+	for (; digit > 0; digit--)
+	{
+		div = 1;
+		for (i = 1; i < digit; i++)
+			div *= 10;
+		_putchar('0' + n / div);
+		n %= div;
+	}
+}
