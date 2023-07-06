252cf6725d57737d90a0c69e5095fcea4eeba83c
Bode <akinbode.adeboyejo@gmail.com>
2023-07-05 18:10:31 -0700

    solving recursions task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
- * is_prime_number - returns 1 if the input integer is a prime number, otherwise return 0.
+ * is_prime_number - returns 1 if the input integer is a prime number,
+ * otherwise return 0.
  * @n: variable of typeinteger
  * Return: 1 if the input integer is a prime number, otherwise return 0.natural
  * square root of a number.
@@ -11,5 +12,5 @@ int is_prime_number(int n)
 		return (1);
 	if (n < 1)
 		return (0);
-	return n % is_prime_number(n - 1)
+	return (n % is_prime_number(n - 1));
 }
