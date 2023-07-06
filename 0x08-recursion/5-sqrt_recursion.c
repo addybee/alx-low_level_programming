0b3acf43d8ef0056dc399da437e9228ce2ddee71
Bode <akinbode.adeboyejo@gmail.com>
2023-07-06 15:48:52 -0700

    midified task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,17 +1,26 @@
 #include "main.h"
 /**
+ * chsqrt - checks for the sqrt of a number
+ * @num: is the number
+ * @i: is the iterator
+ * Return: the sqrt of a number otherwisee -1
+ */
+int chsqrt(int num, int i)
+{
+	if (num == 1)
+		return (num);
+	else if (num == i * i)
+		return (i);
+	else if (i == num - 1 || num < 0)
+		return (-1);
+	return (chsqrt(num, i + 1));
+}
+/**
  * _sqrt_recursion - returns the natural square root of a number.
  * @n: variable of typeinteger
  * Return: natural square root of a number.
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	if (i * i == n)
-	{
-		return (_sqrt_recursion(n - 1));
-	}
+	return (chsqrt(n, 0));
 }
