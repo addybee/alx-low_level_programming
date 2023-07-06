ef6f6634f8dd7af4510b5cc7936b6aea0a40f8de
Bode <akinbode.adeboyejo@gmail.com>
2023-07-06 11:25:20 -0700

    midified task

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,19 @@
 #include "main.h"
 /**
+ * _inp - check if a number is a prime number
+ * @divisor: holds the num use for dividing the num to check if it is divisible
+ * @num: the number to be checked
+ * Return: 1 when prime or 0 otherwise
+ */
+int _inp(int num, int divisor)
+{
+	if (!(num % divisor) || num < 2)
+		return (0);
+	else if (num - 1 == divisor)
+		return (1);
+	return (_inp(num, (divisor + 1)));
+}
+/**
  * is_prime_number - returns 1 if the input integer is a prime number,
  * otherwise return 0.
  * @n: variable of typeinteger
@@ -8,9 +22,5 @@
  */
 int is_prime_number(int n)
 {
-	if (n == 2)
-		return (1);
-	if (n < 1)
-		return (0);
-	return (n % is_prime_number(n - 1));
+	return (_inp(n, 2));
 }
