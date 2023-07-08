bc8b1927873271faebd9fdff344611330fd9d7e1
Bode <akinbode.adeboyejo@gmail.com>
2023-07-08 15:26:27 -0700

    modified task4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * xch - check the string for nondigit
+ * @argc: variable of type int
+ * @argv: pointer to a pointer that point to char
+ * Return: integer
+ */
+int xch(int argc, char **argv)
+{
+	int i;
+	unsigned int j;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; j < strlen(argv[i]); j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+				return (0);
+		}
+	}
+	return (1);
+}
+
 /**
  * main - entry point
  * @argc: variable of type int
@@ -13,19 +37,19 @@ int main(int argc, char **argv)
 	sum = 0;
 	if (argc < 2)
 		printf("%d\n", sum);
-	else
+	else if (xch(argc, argv))
 	{
 		for (i = 1; i < argc; i++)
 		{
 			j = atoi(argv[i]);
-			if (j == 0)
-			{
-				printf("Error\n");
-				return (1);
-			}
 			sum += j;
 		}
 		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
 	}
 	return (0);
 }
