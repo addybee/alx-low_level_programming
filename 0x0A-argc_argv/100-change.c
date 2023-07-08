bed2ea855bba336bd642940badbf368f66f3b5b3
Bode <akinbode.adeboyejo@gmail.com>
2023-07-08 04:00:18 -0700

    modified adv task

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -20,10 +20,12 @@ int main(int argc, char **argv)
 	else
 	{
 		cent = atoi(argv[1]);
-		if (cent <= 0)
+		if (cent < 0)
 			printf("%d\n", j);
 		for (i = 0; i < 5; i++)
 		{
+			if (cent == 0)
+				break;
 			j += (cent / val[i]);
 			cent %= val[i];
 		}
