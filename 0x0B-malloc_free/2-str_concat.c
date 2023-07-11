6975e5f7370b5beaa0be7fd85b2e8883e05b0d8f
Bode <akinbode.adeboyejo@gmail.com>
2023-07-11 13:52:49 -0700

    modified task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,14 +13,16 @@ char *str_concat(char *s1, char *s2)
 
 	for (i = 0; s1[i]; i++)
 		;
+	i--;
 	for (j = 0; s2[j]; j++)
 		;
-	p = malloc((i + j - 1) * sizeof(char));
+	j--;
+	p = malloc((i + j) * sizeof(char));
 	if (p == NULL)
 		return (p);
 	for (k = 0; k < i; k++)
 		p[k] = s1[k];
-	for (i = 0; i <= j; i++)
+	for (i = 0; i < j; i++)
 	{
 		p[k] = s2[i];
 		k++;
