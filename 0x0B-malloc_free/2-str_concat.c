a79e9615793459fccd80315d13b56f45f6395e94
Bode <akinbode.adeboyejo@gmail.com>
2023-07-11 22:59:43 -0700

    modify the indentation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,24 +11,18 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *p;
 
-	if (s1 == NULL)
-	       s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-
+	if (s1 == NULL || s2 == NULL)
+		!s1 ? "" : "";
 	for (i = 0; s1[i]; i++)
 		;
 	for (j = 0; s2[j]; j++)
 		;
-	p = malloc((i + j) * sizeof(char));
+	p = malloc((i + j + 1) * sizeof(char));
 	if (p == NULL)
 		return (p);
 	for (k = 0; k < i; k++)
 		p[k] = s1[k];
 	for (i = 0; i <= j; i++)
-	{
-		p[k] = s2[i];
-		k++;
-	}
+		p[k + i] = s2[i];
 	return (p);
 }
