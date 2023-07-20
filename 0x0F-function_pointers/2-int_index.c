89670a10bd4eca24e7ff632c8ed4c2bd8c08b8f5
Bode <akinbode.adeboyejo@gmail.com>
2023-07-19 17:45:22 -0700

    added README.md and modified tasks

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,3 +1,5 @@
+#include "function_pointers.h"
+#include "stdlib"
 /**
  * int_index - searches for an integer
  * @array: the array to be searched
@@ -9,7 +11,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0 || !array || !cmp)
+	if (size <= 0)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
