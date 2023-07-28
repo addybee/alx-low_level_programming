439366e350c7df6d424de3bade6c1c7625f483b7
addybee <akinbode.adeboyejo@gmail.com>
2023-07-28 01:53:05 +0100

    completed task 1 and minor correctio on task0

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,20 +1,19 @@
 #include "lists.h"
 /**
- * print_list - prints all the elements of a list_t list.
+ * list_len - returns the number of elements in a linked list_t list.
  * @h: holds the singly list
- * Return: the number of nodes
+ * Return: the number of elements in a linked list_t list.
 */
-size_t print_list(const list_t *h)
+size_t list_len(const list_t *h)
 {
     const list_t *temp;
     size_t num_nodes;
 
     num_nodes = 0;
     temp = h;
-    for (; temp->next != NULL; num_nodes++)
+    for (; temp; num_nodes++)
     {
         temp = temp->next;
     }
-    num_nodes++;
     return (num_nodes);
 }
