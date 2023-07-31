12d731915b95f20fd486e22c666ddf59736de7b2
addybee <akinbode.adeboyejo@gmail.com>
2023-08-01 00:21:31 +0100

    first commit for task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * free_listint2 - frees a listint_t list.
+ * @head: pointer to pointer to the listint head
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t * temp;
+	
+	if (!head)
+		return;
+	temp = *head;
+	while (*head->next != NULL)
+	{
+		temp = *head->next;
+		free(*head);
+		*head  = temp;
+	}
+	free(*head);
+	*head = NULL;
+}
