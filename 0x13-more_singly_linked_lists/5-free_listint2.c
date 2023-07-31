da8494a7c4d41e417876712ae4726b970e88f697
addybee <akinbode.adeboyejo@gmail.com>
2023-08-01 00:36:59 +0100

    first commit for task5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 	if (!head)
 		return;
 	temp = *head;
-	while (*head->next != NULL)
+	while ((*head)->next != NULL)
 	{
-		temp = *head->next;
+		temp = (*head)->next;
 		free(*head);
 		*head  = temp;
 	}
