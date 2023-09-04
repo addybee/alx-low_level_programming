d7b8560e9d9000b35f27742d830c1305743967a5
akinbode adeboyejo <akinbode.adeboyejo@gmail.com>
2023-09-04 16:36:04 -0700

    first commit for task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,27 +3,26 @@
  * create_file - creates a file.
  * @filename: name of the file you want to create.
  * @text_content: is a NULL terminated string to write to the file.
- * Return: 1 on success, -1 on failure 
+ * Return: 1 on success, -1 on failure.
 */
 int create_file(const char *filename, char *text_content)
 {
-        int fh;
+	int fh;
 
-        if (!filename)
-                return (-1);
-        if (text_content == NULL)
-        {
-                open(filename, O_CREAT | O_TRUNC, 0600);
-                return (1);
-        }
-        fh = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-        if (fh < 0)
-        {
-               return (-1);
-        }
-        if (!write(fh, text_content, (sizeof(text_content) - 1)))
-        {
-                return (-1);
-        }
-        return (1);
+	if (!filename)
+		return (-1);
+	fh = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fh < 0)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		if (!write(fh, text_content, (sizeof(text_content) - 1)))
+		{
+			return (-1);
+		}
+	}
+	close(fh);
+	return (1);
 }
