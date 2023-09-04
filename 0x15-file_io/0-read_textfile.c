6e2da75f3819b79e57ae4418f44336ccab200b51
akinbode adeboyejo <akinbode.adeboyejo@gmail.com>
2023-09-04 13:38:00 -0700

    first commit for task0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,32 +8,33 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fh;
-        ssize_t result;
-        char *str;
-        
-        if (!filename || !letters)
-                return (0);
-        fh = open(filename, O_RDONLY);
-        if (fh == -1)
-                return (0);
-        str = malloc((letters) * sizeof(char *));
-        if (str == NULL)
-                return (0);        
-        if ((result = read(fh, str, letters)) == -1)
-        {
-                free(str);
-                return (0);
-        }
-        else
-        {
-                if(write(STDOUT_FILENO, str, result) < 0)
-                {
-                        free(str);
-                        return (0);
-                }
-        }
-        close(fh);
-        free (str);
-        return (result);
+	int fh;
+	ssize_t result;
+	char *str;
+
+	if (!filename || !letters)
+		return (0);
+	fh = open(filename, O_RDONLY);
+	if (fh == -1)
+		return (0);
+	str = malloc((letters) * sizeof(char *));
+	if (str == NULL)
+		return (0);
+	result = read(fh, str, letters);
+	if (result == -1)
+	{
+		free(str);
+		return (0);
+	}
+	else
+	{
+		if (write(STDOUT_FILENO, str, result) < 0)
+		{
+			free(str);
+			return (0);
+		}
+	}
+	close(fh);
+	free(str);
+	return (result);
 }
