4006da0d92713ebb033e6569fb37803b30af556c
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-09-05 14:56:43 +0100

    added strlen function

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+size_t _strlen(char *s);
 /**
  * append_text_to_file - appends text at the end of a file.
  * @filename: name of the file you want to append to
@@ -16,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content)
 	{
-		if (write(fh, text_content, (sizeof(text_content) - 1)) == -1)
+		if (write(fh, text_content, _strlen(text_content)) == -1)
 		{
 			close(fh);
 			return (-1);
@@ -24,4 +25,21 @@ int append_text_to_file(const char *filename, char *text_content)
 	}
 	close(fh);
 	return (1);
+}
+/**
+ * _strlen - return the length of a string
+ * @s: pointer variable of char type
+ * Return: length of string
+ */
+size_t _strlen(char *s)
+{
+	size_t counter;
+
+	counter = 0;
+	while (*s != '\0')
+	{
+		counter++;
+		*s++;
+	}
+	return (counter);
 }
