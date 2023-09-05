2ecdadf2244bad83f0095c86d3a7dff9520b1f6a
akinbode adeboyejo <akinbode.adeboyejo@gmail.com>
2023-09-04 17:14:43 -0700

    adjusted the conditional statement

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		if (!write(fh, text_content, (sizeof(text_content) - 1)))
+		if (write(fh, text_content, (sizeof(text_content) - 1)) == -1)
 		{
 			return (-1);
 		}
