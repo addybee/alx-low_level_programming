5f75b47766ce164acdf13a34fc19de6e16c99c82
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-09-05 15:18:00 +0100

    added strlen function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+size_t _strlen(char *s);
 /**
  * create_file - creates a file.
  * @filename: name of the file you want to create.
@@ -18,12 +19,30 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content)
 	{
-		if (write(fh, text_content, (sizeof(text_content) - 1)) == -1)
+		if (write(fh, text_content, _strlen(text_content)) == -1)
 		{
-                        close(fh);
+                        close(fh); 
 			return (-1);
 		}
 	}
 	close(fh);
 	return (1);
+}
+
+/**
+ * _strlen - return the length of a string
+ * @s: pointer variable of char type
+ * Return: length of string
+ */
+size_t _strlen(char *s)
+{
+	size_t counter;
+
+	counter = 0;
+	while (*s != '\0')
+	{
+		counter++;
+		s++;
+	}
+	return (counter);
 }
