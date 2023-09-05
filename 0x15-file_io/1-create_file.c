af0bba331c34d4b7171243224e6b74fbafe20303
akinbode adeboyejo <akinbode.adeboyejo@gmail.com>
2023-09-05 07:46:18 -0700

    adjusted _strlen function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		if (write(fh, text_content, _strlen(text_content)) == -1)
 		{
-                        close(fh); 
+			close(fh);
 			return (-1);
 		}
 	}
