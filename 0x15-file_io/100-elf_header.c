11a9be74a33f7b004f69dc5ec0d6171bb3908059
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-10-02 16:56:59 -0700

    rever to last two commit

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 #include <elf.h>
 #include <sys/types.h>
-void printMagic_Class(Elf32_Ehdr *head);
-void printData_Version(Elf32_Ehdr *head);
-void printABI(Elf32_Ehdr *head);
-void printType_Addr32(Elf32_Ehdr *head);
-void printType_Addr64(int fh_elf, char *el);
+#define BUF 64
+void printMagic_Class(char *head);
+void printData_Version(char *head);
+void printABI(char *head);
+void printType_Addr(char *head);
 /**
  * main - entry point
  * @ac: number of arg
@@ -17,9 +17,9 @@ void printType_Addr64(int fh_elf, char *el);
  */
 int main(int ac, char **av)
 {
-	size_t rdbyte;
+	int rdbyte;
 	int fh_elf;
-	Elf32_Ehdr headf;
+	char headf[BUF];
 
 	if (ac != 2)
 	{
@@ -33,32 +33,28 @@ int main(int ac, char **av)
 			       av[1]);
 		exit(99);
 	}
-	rdbyte = read(fh_elf, &headf, sizeof(headf));
-	if (rdbyte != sizeof(headf))
+	rdbyte = read(fh_elf, headf, BUF);
+	if (rdbyte < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 				av[1]);
-		close(fh_elf);
 		exit(98);
 	}
-	if (headf.e_ident[EI_MAG0] != 0x7f || headf.e_ident[EI_MAG1] != 'E' ||
-			headf.e_ident[EI_MAG2] != 'L' || headf.e_ident[EI_MAG3]
-			!= 'F')
+	if (headf[0] != 0x7f || headf[1] != 'E' || headf[2] != 'L' || headf[3]
+	!= 'F')
 	{
 		dprintf(STDERR_FILENO, "Error: %s is not a valid ELF file.\n",
 				av[1]);
 		close(fh_elf);
 		exit(98);
 	}
-		/* display the header file */
+	lseek(fh_elf, 0, SEEK_SET);
+	/* display the header file */
 	printf("ELF Header:\n");
-	printMagic_Class(&headf);
-	printData_Version(&headf);
-	printABI(&headf);
-	if (headf.e_ident[EI_CLASS] == ELFCLASS32)
-		printType_Addr32(&headf);
-	else
-		printType_Addr64(fh_elf, av[1]);
+	printMagic_Class(headf);
+	printData_Version(headf);
+	printABI(headf);
+	printType_Addr(headf);
 	close(fh_elf);
 	return (1);
 }
@@ -67,25 +63,25 @@ int main(int ac, char **av)
  * @head: header information
  * Return: void
  */
-void printMagic_Class(Elf32_Ehdr *head)
+void printMagic_Class(char *head)
 {
 	int i;
 
 	printf("  Magic:    ");
-	for (i = EI_MAG0; i < EI_NIDENT; i++)
+	for (i = 0; i < 16; i++)
 	{
-		printf("%02x", head->e_ident[i]);
-		if (i != (EI_NIDENT - 1))
+		printf("%02x", head[i]);
+		if (i != 15)
 			printf(" ");
 	}
 	printf("\n");
 	printf("  %-36s", "Class:");
-	if (head->e_ident[EI_CLASS] == ELFCLASS32)
+	if (head[4] == 1)
 		printf("ELF32\n");
-	else if (head->e_ident[EI_CLASS] == ELFCLASS64)
+	else if (head[4] == 2)
 		printf("ELF64\n");
 	else
-		printf("<unknown: %02X>\n",  head->e_ident[EI_CLASS]);
+		printf("<unknown: %02X>\n",  head[4]);
 }
 
 /**
@@ -94,26 +90,29 @@ void printMagic_Class(Elf32_Ehdr *head)
  * @head: header information
  * Return: nothing
  */
-void printData_Version(Elf32_Ehdr *head)
+void printData_Version(char *head)
 {
 	printf("  %-36s", "Data:");
-	if (head->e_ident[EI_DATA] == ELFDATA2LSB)
+	if (head[5] == 1)
 		printf("2's complement, little endian\n");
-	else if (head->e_ident[EI_DATA] == ELFDATA2MSB)
+	else if (head[5] == 2)
 		printf("2's complement, big endian\n");
 	else
-		printf("<unknown: %02X>\n", head->e_ident[EI_DATA]);
+		printf("<unknown: %02X>\n", head[5]);
 	/* prints version info from elf header */
 	printf("  %-36s", "Version:");
-	if (head->e_ident[EI_DATA] == EV_NONE)
-		printf("<unknown %%lx>");
-	else
+	if (head[6] <= EV_CURRENT)
 	{
-		printf("%d", head->e_ident[EI_DATA]);
-		if (head->e_ident[EI_DATA] == EV_CURRENT)
+		printf("%d", head[6]);
+		if (head[6] == EV_CURRENT)
 			printf(" (current)\n");
 		else
 			printf("\n");
+
+	}
+	else
+	{
+		printf("<unknown %%lx>");
 	}
 }
 
@@ -122,93 +121,62 @@ void printData_Version(Elf32_Ehdr *head)
  * @head: header information
  * Return: void
  */
-void printABI(Elf32_Ehdr *head)
+void printABI(char *head)
 {
 	printf("  %-36s", "OS/ABI:");
-	switch (head->e_ident[EI_OSABI])
+	switch (head[7])
 	{
-	case ELFOSABI_SYSV:
+	case 0:
 		printf("UNIX - System V\n");
 		break;
-	case ELFOSABI_HPUX:
+	case 1:
 		printf("UNIX - HP-UX\n");
 		break;
-	case ELFOSABI_NETBSD:
+	case 2:
 		printf("UNIX - NetBSD\n");
 		break;
-	case ELFOSABI_LINUX:
+	case 3:
 		printf("UNIX - Linux\n");
 		break;
-	case ELFOSABI_SOLARIS:
+	case 6:
 		printf("UNIX - Solaris\n");
 		break;
-	case ELFOSABI_IRIX:
+	case 8:
 		printf("UNIX - IRIX\n");
 		break;
-	case ELFOSABI_FREEBSD:
+	case 9:
 		printf("UNIX - FreeBSD\n");
 		break;
-	case ELFOSABI_TRU64:
+	case 10:
 		printf("UNIX - Tru64\n");
 		break;
 	default:
-		printf("<unknown: %02x>\n", head->e_ident[EI_OSABI]);
+		printf("<unknown: %02x>\n", head[7]);
 		break;
 	}
-	printf("  %-36s%d\n", "ABI Version:", head->e_ident[EI_ABIVERSION]);
+	printf("  %-36s%d\n", "ABI Version:", head[8]);
 }
 
 /**
- * printType_Addr32 - prints elf filetype and entry address from header 32 bit
+ * printType_Addr - prints elf filetype and entry address from header
  * @head: header information
  * Return: void
  */
-void printType_Addr32(Elf32_Ehdr *head)
+void printType_Addr(char *head)
 {
-	printf("  %-36s", "Type:");
-	if (head->e_type == ET_REL)
-		printf("REL (Relocatable file)\n");
-	else if (head->e_type == ET_EXEC)
-		printf("EXEC (Executable file)\n");
-	else if (head->e_type == ET_DYN)
-		printf("DYN (Shared object file)\n");
-	else if (head->e_type == ET_CORE)
-		printf("CORE (Core file)\n");
-	else
-		printf("NONE <unknown>: %02x\n",  head->e_type);
-	printf("  %-36s0x%X\n", "Entry point address:", head->e_entry);
-}
-/**
- * printType_Addr64 - prints elf filetype and entry address from header 64 bit
- * @fh_elf: file decriptor
- * @el: is the elf header file
- * Return: void
- */
-void printType_Addr64(int fh_elf, char *el)
-{
-	Elf64_Ehdr header;
-	Elf64_Ehdr *head;
-	size_t rdbyte;
+	u_int16_t tp;
 
-	lseek(fh_elf, 0, SEEK_SET);
-	head = &header;
-	rdbyte = read(fh_elf, head, sizeof(header));
-	if (rdbyte != sizeof(header))
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", el);
-		close(fh_elf);
-		exit(98);
-	}
+	tp = *((u_int16_t *)(head + 16));
 	printf("  %-36s", "Type:");
-	if (head->e_type == ET_REL)
+	if (tp == 1)
 		printf("REL (Relocatable file)\n");
-	else if (head->e_type == ET_EXEC)
+	else if (tp == 2)
 		printf("EXEC (Executable file)\n");
-	else if (head->e_type == ET_DYN)
+	else if (tp == 3)
 		printf("DYN (Shared object file)\n");
-	else if (head->e_type == ET_CORE)
+	else if (tp == 4)
 		printf("CORE (Core file)\n");
 	else
-		printf("NONE <unknown>: %02x\n",  head->e_type);
-	printf("  %-36s0x%lX\n", "Entry point address:", head->e_entry);
+		printf("NONE <unknown>: %02x\n",  tp);
+	printf("  %-36s0x%X\n", "Entry point address:", *((u_int32_t *)(head + 24)));
 }
