222acdc8105d99656157c34f733e9f114f90c527
Akinbode Adeboyejo <akinbode.adeboyejo@gmail.com>
2023-10-03 14:22:31 -0700

    reduce the string to be 35 character instead of 36

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -4,11 +4,11 @@
 #include <stdlib.h>
 #include <elf.h>
 #include <sys/types.h>
-#define BUF 64
-void printMagic_Class(char *head);
-void printData_Version(char *head);
-void printABI(char *head);
-void printType_Addr(char *head);
+void printMagic_Class(Elf64_Ehdr *head);
+void printData_Version(Elf64_Ehdr *head);
+void printABI(Elf64_Ehdr *head);
+void printType(Elf64_Ehdr *head);
+void printAddr(Elf64_Ehdr *head);
 /**
  * main - entry point
  * @ac: number of arg
@@ -17,9 +17,9 @@ void printType_Addr(char *head);
  */
 int main(int ac, char **av)
 {
-	int rdbyte;
+	size_t rdbyte;
 	int fh_elf;
-	char headf[BUF];
+	Elf64_Ehdr headf;
 
 	if (ac != 2)
 	{
@@ -33,28 +33,30 @@ int main(int ac, char **av)
 			       av[1]);
 		exit(99);
 	}
-	rdbyte = read(fh_elf, headf, BUF);
-	if (rdbyte < 0)
+	rdbyte = read(fh_elf, &headf, sizeof(headf));
+	if (rdbyte != sizeof(headf))
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
 				av[1]);
+		close(fh_elf);
 		exit(98);
 	}
-	if (headf[0] != 0x7f || headf[1] != 'E' || headf[2] != 'L' || headf[3]
-	!= 'F')
+	if (headf.e_ident[EI_MAG0] != 0x7f || headf.e_ident[EI_MAG1] != 'E' ||
+			headf.e_ident[EI_MAG2] != 'L' || headf.e_ident[EI_MAG3]
+			!= 'F')
 	{
 		dprintf(STDERR_FILENO, "Error: %s is not a valid ELF file.\n",
 				av[1]);
 		close(fh_elf);
 		exit(98);
 	}
-	lseek(fh_elf, 0, SEEK_SET);
-	/* display the header file */
+		/* display the header file */
 	printf("ELF Header:\n");
-	printMagic_Class(headf);
-	printData_Version(headf);
-	printABI(headf);
-	printType_Addr(headf);
+	printMagic_Class(&headf);
+	printData_Version(&headf);
+	printABI(&headf);
+	printType(&headf);
+	printAddr(&headf);
 	close(fh_elf);
 	return (1);
 }
@@ -63,25 +65,27 @@ int main(int ac, char **av)
  * @head: header information
  * Return: void
  */
-void printMagic_Class(char *head)
+void printMagic_Class(Elf64_Ehdr *head)
 {
 	int i;
 
 	printf("  Magic:   ");
-	for (i = 0; i < 16; i++)
+	for (i = EI_MAG0; i < EI_NIDENT; i++)
 	{
-		printf("%02x", head[i]);
-		if (i != 15)
+		printf("%02x", head->e_ident[i]);
+		if (i != (EI_NIDENT - 1))
 			printf(" ");
 	}
 	printf("\n");
 	printf("  %-35s", "Class:");
-	if (head[4] == 1)
+	if (head->e_ident[EI_CLASS] == ELFCLASS32)
 		printf("ELF32\n");
-	else if (head[4] == 2)
+	else if (head->e_ident[EI_CLASS] == ELFCLASS64)
 		printf("ELF64\n");
+	else if (head->e_ident[EI_CLASS] == ELFCLASSNONE)
+		printf("none\n");
 	else
-		printf("<unknown: %02X>\n",  head[4]);
+		printf("<unknown: %02X>\n",  head->e_ident[EI_CLASS]);
 }
 
 /**
@@ -90,30 +94,24 @@ void printMagic_Class(char *head)
  * @head: header information
  * Return: nothing
  */
-void printData_Version(char *head)
+void printData_Version(Elf64_Ehdr *head)
 {
 	printf("  %-35s", "Data:");
-	if (head[5] == 1)
+	if (head->e_ident[EI_DATA] == ELFDATA2LSB)
 		printf("2's complement, little endian\n");
-	else if (head[5] == 2)
+	else if (head->e_ident[EI_DATA] == ELFDATA2MSB)
 		printf("2's complement, big endian\n");
+	else if (head->e_ident[EI_DATA] == ELFDATANONE)
+		printf("none\n");
 	else
-		printf("<unknown: %02X>\n", head[5]);
+		printf("<unknown: %02X>\n", head->e_ident[EI_DATA]);
 	/* prints version info from elf header */
 	printf("  %-35s", "Version:");
-	if (head[6] <= EV_CURRENT)
-	{
-		printf("%d", head[6]);
-		if (head[6] == EV_CURRENT)
-			printf(" (current)\n");
-		else
-			printf("\n");
-
-	}
+	printf("%d", head->e_ident[EI_DATA]);
+	if (head->e_ident[EI_DATA] == EV_CURRENT)
+		printf(" (current)\n");
 	else
-	{
-		printf("<unknown %%lx>");
-	}
+		printf("\n");
 }
 
 /**
@@ -121,62 +119,91 @@ void printData_Version(char *head)
  * @head: header information
  * Return: void
  */
-void printABI(char *head)
+void printABI(Elf64_Ehdr *head)
 {
 	printf("  %-35s", "OS/ABI:");
-	switch (head[7])
+	switch (head->e_ident[EI_OSABI])
 	{
-	case 0:
+	case ELFOSABI_NONE:
 		printf("UNIX - System V\n");
 		break;
-	case 1:
+	case ELFOSABI_HPUX:
 		printf("UNIX - HP-UX\n");
 		break;
-	case 2:
+	case ELFOSABI_NETBSD:
 		printf("UNIX - NetBSD\n");
 		break;
-	case 3:
+	case ELFOSABI_LINUX:
 		printf("UNIX - Linux\n");
 		break;
-	case 6:
+	case ELFOSABI_SOLARIS:
 		printf("UNIX - Solaris\n");
 		break;
-	case 8:
+	case ELFOSABI_IRIX:
 		printf("UNIX - IRIX\n");
 		break;
-	case 9:
+	case ELFOSABI_FREEBSD:
 		printf("UNIX - FreeBSD\n");
 		break;
-	case 10:
+	case ELFOSABI_TRU64:
 		printf("UNIX - Tru64\n");
 		break;
+	case ELFOSABI_ARM:
+		printf("ARM\n");
+		break;
+	case ELFOSABI_STANDALONE:
+		printf("Stand-alone APP\n");
+		break;
 	default:
-		printf("<unknown: %02x>\n", head[7]);
+		printf("<unknown: %02x>\n", head->e_ident[EI_OSABI]);
 		break;
 	}
-	printf("  %-35s%d\n", "ABI Version:", head[8]);
+	printf("  %-35s%d\n", "ABI Version:", head->e_ident[EI_ABIVERSION]);
 }
 
 /**
- * printType_Addr - prints elf filetype and entry address from header
+ * printType - prints elf filetype
  * @head: header information
  * Return: void
  */
-void printType_Addr(char *head)
+void printType(Elf64_Ehdr *head)
 {
-	u_int16_t tp;
-
-	tp = *((u_int16_t *)(head + 16));
+	if (head->e_ident[EI_DATA] == ELFDATA2MSB)
+		head->e_type >>= 8;
 	printf("  %-35s", "Type:");
-	if (tp == 1)
+	if (head->e_type == ET_REL)
 		printf("REL (Relocatable file)\n");
-	else if (tp == 2)
+	else if (head->e_type == ET_EXEC)
 		printf("EXEC (Executable file)\n");
-	else if (tp == 3)
+	else if (head->e_type == ET_DYN)
 		printf("DYN (Shared object file)\n");
-	else if (tp == 4)
+	else if (head->e_type == ET_CORE)
 		printf("CORE (Core file)\n");
+	else if (head->e_type == ET_NONE)
+		printf("NONE (None)\n");
 	else
-		printf("NONE <unknown>: %02x\n",  tp);
-	printf("  %-35s0x%X\n", "Entry point address:", *((u_int32_t *)(head + 24)));
+		printf("<unknown: %02x>\n",  head->e_type);
+}
+/**
+ * printAddr - prints entry address from header
+ * @head: header information
+ *  Return: void
+ */
+void printAddr(Elf64_Ehdr *head)
+{
+	if (head->e_ident[EI_DATA] == ELFDATA2MSB)
+	{
+		head->e_entry = ((head->e_entry << 8) & 0xFF00FF00) |
+			((head->e_entry >> 8) & 0xFF00FF);
+		head->e_entry = (head->e_entry << 16) | (head->e_entry >> 16);
+	}
+	printf("  %-35s", "Entry point address:");
+	if (head->e_ident[EI_CLASS] == ELFCLASS32)
+	{
+		printf("%#x\n",	(unsigned int)head->e_entry);
+	}
+	else
+	{
+		printf("%#lx\n", head->e_entry);
+	}
 }
