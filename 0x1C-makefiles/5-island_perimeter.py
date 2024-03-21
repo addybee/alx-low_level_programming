467776eeb74c382bd1b4eafbdc186cba04db60cb
Bode Adeboyejo <akinbode.adeboyejo@gmail.com>
2024-03-21 14:20:32 -0700

    finished up all task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,27 +12,28 @@ def island_perimeter(grid):
     Returns:
         int: perimeter of the island
     """
-    total_perimeter = 0
 
-    for i in range(len(grid)):
+    perimeter = 0
+    grid_len = len(grid)
+    row_len = len(grid[0])
 
-        num_of_land_cell_in_row = 0
-        num_of_land_cell_ontop_row = 0
+    for i in range(grid_len):
+    
+        for j in range(row_len):
 
-        for j in range(len(grid[i])):
+            if grid[i][j]:
 
-            if i == 0 and grid[i][j]:
-                num_of_land_cell_in_row += 1
-            elif grid[i][j]:
-                num_of_land_cell_in_row += 1
-                if grid[i - 1][j]:
-                    num_of_land_cell_ontop_row += 1
+                if i == 0 or i == grid_len -1:
+                    perimeter+=1
+                if j == 0 or j == row_len - 1:
+                    perimeter += 1
+                if i > 0 and not grid[i - 1][j]:
+                    perimeter += 1
+                if j < (row_len - 1) and not grid[i][j + 1]:
+                    perimeter += 1
+                if j > 0 and not grid[i][j - 1]:
+                    perimeter += 1
+                if i < (grid_len - 1) and not grid[i + 1][j]:
+                    perimeter += 1
 
-        if i == 0 and num_of_land_cell_in_row > 0:
-            total_perimeter = (num_of_land_cell_in_row * 2) + 2
-        elif num_of_land_cell_in_row > 0:
-            row_perimeter = (num_of_land_cell_in_row * 2) + 2
-            line_btw_top_row = (num_of_land_cell_ontop_row * 2)
-            net_row_perimeter = row_perimeter - line_btw_top_row
-            total_perimeter += net_row_perimeter
-    return total_perimeter
+    return perimeter
