96a425f1ff892c93f609630a41d2fc171fe489dd
Adeboyejo Akinbode <akinbode.adeboyejo@gmail.com>
2024-05-10 03:53:43 +0100

    fix task 105 bug

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 int min(int a, int b);
-listint_t *jump_list_help(listint_t **array, size_t size, int value);
+listint_t *node_at_index(listint_t *list, size_t index);
 
 /**
  * jump_list - Searches for a value in a list using
@@ -18,46 +18,19 @@ listint_t *jump_list_help(listint_t **array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
 	size_t i;
-	listint_t **arr;
+	int step, prev;
 
 	if (!list || !size)
 		return (NULL);
-
-	arr = (listint_t **)malloc(size * sizeof(listint_t *));
-	if (!arr)
-		return (NULL);
-
-	for (i = 0; i < size; i++)
-	{
-		arr[i] =  list;
-		list = list->next;
-	}
-	return (jump_list_help(arr, size, value));
-}
-
-
-
-/**
- * jump_list_help - help Searches for a value in a list using
- * jump search algorithm.
- *
- * @array: Pointer to the node of the list to search.
- * @size: Number of elements in the list.
- * @value: Value to search for.
- *
- * Return: node of the value if found, otherwise NULL.
- */
-listint_t *jump_list_help(listint_t **array, size_t size, int value)
-{
-	int step, prev, i;
 
 	/* Calculate the step size */
 	step = sqrt(size);
 
 	/* Initialize previous index */
 	prev = 0;
+
 	/* Perform jump search */
-	while (array[min(step, (size - 1))]->n < value)
+	while (node_at_index(list, min(step, (size - 1)))->n < value)
 	{
 		prev = step;
 		step += sqrt(size);
@@ -69,20 +42,24 @@ listint_t *jump_list_help(listint_t **array, size_t size, int value)
 			break;
 		}
 		/* Print value being checked */
-		printf("Value checked array[%d] = [%d]\n",
-		prev, array[prev]->n);
+		printf("Value checked array[%d] = [%d]\n", prev,
+		 node_at_index(list, prev)->n);
 	}
+	/* Print value being checked */
+	printf("Value checked array[%d] = [%d]\n", min(step, (size - 1)),
+		node_at_index(list, min(step, (size - 1)))->n);
 	printf("Value found between indexes [%d] and [%d]\n", prev, step);
 	/* Perform linear search within the block */
-	for (i = prev; i < min(step + 1, size); i++)
+	for (i = prev; i < (size_t)min(step + 1, size); i++)
 	{
+		listint_t *val = node_at_index(list, i);
 		/* Print value being checked */
-		printf("Value checked array[%d] = [%d]\n", i, array[i]->n);
+		printf("Value checked array[%lu] = [%d]\n", i, val->n);
 
 		/* If value is found, return its index */
-		if (array[i]->n == value)
+		if (val->n == value)
 		{
-			return (array[i]);
+			return (val);
 		}
 	}
 	/* Return -1 if value is not found */
@@ -105,4 +82,25 @@ int min(int a, int b)
 		return (a); /* Return a if b is greater */
 	else
 		return (b); /* Return b if b is less than or equal to a */
+}
+
+/**
+ * node_at_index - Returns a pointer to the node at the given
+ * index in a linked list.
+ *
+ * @list: Pointer to the head of the linked list.
+ * @index: Index of the node to retrieve.
+ *
+ * Return: A pointer to the node at the given index, or
+ * NULL if the index is out of bounds.
+ */
+listint_t *node_at_index(listint_t *list, size_t index)
+{
+	listint_t *res_node = list;
+
+	while (index--)
+	{
+		res_node = res_node->next;
+	}
+	return (res_node);
 }
